f4126d6bc135b880f8b38082afcd7b460f6b172c
Bartu Baş <velenrendlich@gmail.com>
2022-02-26 16:21:24 +0300

    Add: Initial capacity tests.

---
+++ StackOnlyList/Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StackOnlyList
+{
+	public class Tests
+	{
+		[Test]
+		public void AddOneElement()
+		{
+			using var list = new StackOnlyList<int>(1);
+			list.Add(2);
+			
+			Assert.AreEqual(2, list[0]);
+		}
+
+		[TestCase(-1)]
+		[TestCase(-10)]
+		[TestCase(-24382934)]
+		[TestCase(int.MinValue)]
+		public void NegativeCapacityThrows(int negativeCapacity)
+		{
+			Assert.Throws<InvalidOperationException>(() =>
+			{
+				using var list = new StackOnlyList<int>(negativeCapacity);
+			});
+		}
+		
+		[Test]
+		public void ZeroCapacityThrows()
+		{
+			Assert.Throws<InvalidOperationException>(() =>
+			{
+				using var list = new StackOnlyList<int>(0);
+			});
+		}
+
+		[TestCase(1)]
+		[TestCase(10)]
+		[TestCase(34287)]
+		// [TestCase(int.MaxValue)]
+		public void PositiveCapacityDoesNotThrow(int capacity)
+		{
+			Assert.DoesNotThrow(() =>
+			{
+				using var list = new StackOnlyList<int>(capacity);
+			});
+		}
+	}
+}
