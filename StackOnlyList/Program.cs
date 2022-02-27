40058883df520356ad24f8c405e012364d1b6207
Bartu Baş <velenrendlich@gmail.com>
2022-02-27 11:17:31 +0300

    Move class.

--- StackOnlyList/Program.cs
+++ StackOnlyList/Program.cs
@@ -2,14 +2,6 @@
 
 namespace StackOnlyList
 {
-	public static class Util
-	{
-		public static unsafe int SafeStackSize<T>() where T : unmanaged
-		{
-			return 256 / sizeof(T);
-		}
-	}
-	
 	class Program
 	{
 		static void Main(string[] args)
---
+++ StackOnlyList/Util.cs
@@ -0,0 +1,10 @@
+﻿namespace StackOnlyList
+{
+	public static class Util
+	{
+		public static unsafe int SafeStackSize<T>() where T : unmanaged
+		{
+			return 512 / sizeof(T);
+		}
+	}
+}
