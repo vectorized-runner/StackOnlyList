e63dd84830d1319c61b2374cb58c6b34a3ddc219
Bartu Baş <velenrendlich@gmail.com>
2022-02-27 11:18:14 +0300

    Fix: Make benchmarkdotnet run with NUnit

--- StackOnlyList/Program.cs
+++ StackOnlyList/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
 
 namespace StackOnlyList
 {
@@ -7,6 +13,16 @@ namespace StackOnlyList
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			
+			// Don't care if NUnit is optimized or not
+			var config = new ManualConfig()
+			             .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+			             // .AddValidator(JitOptimizationsValidator.DontFailOnError)
+			             .AddLogger(ConsoleLogger.Default)
+			             .AddExporter(RPlotExporter.Default, CsvExporter.Default)
+			             .AddColumnProvider(DefaultColumnProviders.Instance);
+
+			var summary = BenchmarkRunner.Run<Benchmarks>(config);
 		}
 	}
 }
