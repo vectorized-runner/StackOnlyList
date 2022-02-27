3a9b23dd67466e9f825b1347e7a1fde7837651fb
Bartu Baş <velenrendlich@gmail.com>
2022-02-27 12:49:46 +0300

    Add: List.Remove tests

--- StackOnlyList/Tests.cs
+++ StackOnlyList/Tests.cs
@@ -22,6 +22,63 @@ namespace StackOnlyList
 		// }
 
 		[Test]
+		public void RemoveFromMiddle()
+		{
+			using var list = new StackOnlyList<int>(10);
+			list.Add(0);
+			list.Add(2);
+			list.Add(4);
+			list.Add(6);
+			list.Add(8);
+
+			list.Remove(4);
+
+			Assert.AreEqual(0, list[0]);
+			Assert.AreEqual(2, list[1]);
+			Assert.AreEqual(6, list[2]);
+			Assert.AreEqual(8, list[3]);
+			Assert.AreEqual(4, list.Count);
+		}
+		
+		[Test]
+		public void RemoveFirst()
+		{
+			using var list = new StackOnlyList<int>(10);
+			list.Add(0);
+			list.Add(2);
+			list.Add(4);
+			list.Add(6);
+			list.Add(8);
+
+			list.Remove(0);
+
+			Assert.AreEqual(2, list[0]);
+			Assert.AreEqual(4, list[1]);
+			Assert.AreEqual(6, list[2]);
+			Assert.AreEqual(8, list[3]);
+			Assert.AreEqual(4, list.Count);
+		}
+		
+		[Test]
+		public void RemoveFromLast()
+		{
+			using var list = new StackOnlyList<int>(10);
+			list.Add(0);
+			list.Add(2);
+			list.Add(4);
+			list.Add(6);
+			list.Add(8);
+
+			list.Remove(8);
+
+			Assert.AreEqual(0, list[0]);
+			Assert.AreEqual(2, list[1]);
+			Assert.AreEqual(4, list[2]);
+			Assert.AreEqual(6, list[3]);
+			Assert.AreEqual(4, list.Count);
+		}
+
+		[Test]
 		public void CountNotDecreasedAfterRemoveNonExisting()
 		{
 			using var list = new StackOnlyList<int>(10);
